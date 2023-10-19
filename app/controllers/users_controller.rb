8b5fefe4888798de8ab9358c6e39e0472e6b52cb
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-10-19 13:06:22 -0400

    add error messages for registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,13 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+      if User.find_by_email(user.email)
+        flash[:alert] = "Email exists in database."
+        redirect_to '/signup'
+      else
+        flash[:alert] = "Error creating user."
+        redirect_to '/signup'
+      end
     end
   end
 
